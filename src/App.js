1d4b642df805a88f36af460d12d002875bc8b317
guille garcia <ggarciacardiel@gmail.com>
2023-02-27 19:42:46 +0100

    it2

--- src/App.js
+++ src/App.js
@@ -11,13 +11,17 @@ function App() {
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
         </tr>
           {contacts.map((elem, i) => 
             i < 5 ? 
-            <tr key={i}>
+            <tr key={elem.id}>
               <td><img width='90px' src={elem.pictureUrl} alt="profileImage"/></td>
               <td>{elem.name}</td>
               <td>{elem.popularity}</td>
+              <td>{elem.wonOscar && <span>🏆</span>}</td>
+              <td>{elem.wonEmmy && <span>🏆</span>}</td>
             </tr> 
             : null
           )}
